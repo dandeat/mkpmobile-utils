fd25b80d831a4ad74b12cbfcb581d8afd97d6dfc
Aditya Priyambodo <adityapriyambodo@Adityas-MacBook-Air.local>
2022-07-21 13:00:40 +0700

    add new utils

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -2,6 +2,8 @@ package mkpmobileutils
 
 import (
 	"crypto"
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/hmac"
 	"crypto/rand"
 	"crypto/rsa"
@@ -16,11 +18,14 @@ import (
 	"errors"
 	"fmt"
 	"math/big"
+	"reflect"
+	"strconv"
 	"strings"
 	"time"
 	"unicode"
 
 	"github.com/labstack/echo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 //Timestamp RFC3339
@@ -136,6 +141,140 @@ func Int32ToString(n int32) string {
 			return string(buf[pos:])
 		}
 	}
+}
+
+func ReplaceSQL(old, searchPattern string) string {
+	tmpCount := strings.Count(old, searchPattern)
+	for m := 1; m <= tmpCount; m++ {
+		old = strings.Replace(old, searchPattern, "$"+strconv.Itoa(m), 1)
+	}
+	return old
+}
+
+func ValBlankOrNull(request interface{}, keyName ...string) error {
+	var params interface{}
+	_ = json.Unmarshal([]byte(ToString(request)), &params)
+	paramsValue := params.(map[string]interface{})
+
+	for idx := range keyName {
+		name := keyName[idx]
+		if len(strings.TrimSpace(paramsValue[name].(string))) == 0 {
+			return errors.New(fmt.Sprintf("%s must be filled", name))
+		}
+	}
+
+	return nil
+}
+
+func InArray(v interface{}, in interface{}) (ok bool, i int) {
+	val := reflect.Indirect(reflect.ValueOf(in))
+	switch val.Kind() {
+	case reflect.Slice, reflect.Array:
+		for ; i < val.Len(); i++ {
+			if ok = v == val.Index(i).Interface(); ok {
+				return
+			}
+		}
+	}
+	return
+}
+
+// Make hash
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+// Check hash
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func ToString(i interface{}) string {
+	log, _ := json.Marshal(i)
+	logString := string(log)
+
+	return logString
+}
+
+func CreateCredential(secret string, value string) (result string, err error) {
+
+	// Create a new HMAC by defining the hash type and the key (as byte array)
+	h := hmac.New(sha256.New, []byte(secret))
+
+	// Write Data to it
+	h.Write([]byte(value))
+
+	// Get result and encode as hexadecimal string
+	sha := hex.EncodeToString(h.Sum(nil))
+
+	db, err := decodeHex([]byte(sha))
+	if err != nil {
+		fmt.Printf("failed to decode hex: %s", err)
+		return
+	}
+
+	f := base64Encode(db)
+
+	return string(f), err
+}
+
+func base64Encode(input []byte) []byte {
+	eb := make([]byte, base64.StdEncoding.EncodedLen(len(input)))
+	base64.StdEncoding.Encode(eb, input)
+
+	return eb
+}
+
+func decodeHex(input []byte) ([]byte, error) {
+	db := make([]byte, hex.DecodedLen(len(input)))
+	_, err := hex.Decode(db, input)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func Base64ToHex(s string) string {
+	p, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		// handle error
+	}
+	h := hex.EncodeToString(p)
+	return h
+}
+
+// Decrypt from base64 to decrypted string
+func Aes256Decrypt(cryptoText string, saltKey ...interface{}) (interface{}, error) {
+	var result interface{}
+	keyText := ""
+	if len(saltKey) > 0 {
+		keyText = saltKey[0].(string)
+	}
+	key := []byte(keyText)
+	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return result, err
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		return result, err
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(ciphertext, ciphertext)
+	unMarshall := json.Unmarshal(ciphertext, &result)
+	fmt.Println(unMarshall)
+	return result, nil
 }
 
 // -------------------------------- Digital Signature --------------------------------
